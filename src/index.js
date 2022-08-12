b35a15a47a7bcbd5fec981d4d3cb305d2b08f192
CharlesGiovanniello <charles.giovanniello@gmail.com>
2022-08-12 15:09:03 -0400

    Removing unnecessary code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const Parse = require('parse/node')
 const app = require('./app')
 const port = process.env.PORT
 
-Parse.initialize(APP_ID,JAVASCRIPT_ID)
 app.listen(port, ()=>{
     console.log("Server is up on port " + port)
 } )
